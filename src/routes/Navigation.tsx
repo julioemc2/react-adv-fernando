d2b9a0bc6cb7108858d3d33db0601a19952080bf
julioemc2 <julioemc2@gmail.com>
2023-10-18 17:58:26 -0400

    iniciando formik

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
 
 import logo from '../logo.svg'
+import { RegisterPage } from '../03-forms/pages/RegisterPage';
 
 export const Navigation = () => {
     return (
@@ -11,7 +12,7 @@ export const Navigation = () => {
                     <img src={ logo } alt="React Logo" />
                     <ul>
                         <li>
-                            <NavLink to="/home" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Home</NavLink>
+                            <NavLink to="/register" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Register Page</NavLink>
                         </li>
                         <li>
                             <NavLink to="/about" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>About</NavLink>
@@ -24,7 +25,7 @@ export const Navigation = () => {
 
 
                 <Routes>
-                    <Route path="about" element={ <h1>About Page</h1> } />
+                    <Route path="/register" element={ <RegisterPage /> } />
                     <Route path="users" element={ <h1>Users Page</h1> } />
                     <Route path="home" element={ <h1>Home Page</h1> } />
                     
